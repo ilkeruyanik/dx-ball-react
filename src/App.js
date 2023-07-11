946b28967fe3f3d34123c94477e8382b9c13ca8a
İlker Uyanık <ilker.uyanik11@gmail.com>
2023-07-11 15:18:43 +0300

    Project started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="full-width h-screen">
+          <div className="w-[1200px] bg-black m-auto h-screen border-x border-solid">
+
+          </div>
+      </div>
+  )
 }
 
 export default App;
